9913267a0271022eea756c48d5f22d913ecc1fc6
Okinbird <okinyinka@yahoo.co.uk>
2022-08-06 09:41:57 -0700

    Twenty-two commit

--- _printf.c
+++ _printf.c
@@ -2,41 +2,6 @@
 #include <stdlib.h>
 
 /**
- * check_for_specifiers - Checks if there is a valid format specifier
- * @format: Possible format specifier
- *
- * Return: Pointer to valid function or NULL
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-unsigned int i;
-print_t p[] = {
-{"c", print_c},
-{"s", print_s},
-{"i", print_i},
-{"d", print_d},
-{"u", print_u},
-{"b", print_b},
-{"o", print_o},
-{"x", print_x},
-{"X", print_X},
-{"p", print_p},
-{"S", print_S},
-{"r", print_r},
-{"R", print_R},
-{NULL, NULL}
-};
-for (i = 0; p[i].t != NULL; i++)
-{
-if (*(p[i].t) == *format)
-{
-break;
-}
-}
-return (p[i].f);
-}
-
-/**
  * _printf - Prints anything
  * @format: List of argument types passsed to the function
  *
@@ -44,38 +9,16 @@ return (p[i].f);
  */
 int _printf(const char *format, ...)
 {
-unsigned int i = 0, count = 0;
-va_list list;
-int (*f)(va_list);
-
-if (format == NULL)
-return (-1);
-va_start(list, format);
-while (format[i])
-{
-for (; format[i] != '%' && format[i]; i++)
-{
-_putchar(format[i]);
-count++;
-}
-if (!format[i])
-return (count);
-f = check_for_specifiers(&format[i + 1]);
-if (f != NULL)
-{
-count += f(list);
-i += 2;
-continue;
-}
-if (!format[i + 1])
-return (-1);
-_putchar(format[i]);
-count++;
-if (format[i + 1] == '%')
-i += 2;
-else
-i++;
-}
-va_end(list);
-return (count);
+  int size;
+  va_list args;
+  if (format == NULL)
+    return (-1);
+  size = _strlen(format);
+  if (size <= 0)
+    return (0);
+  va_start(args, format);
+  size = handler(format, args);
+  _putchar(-1);
+  va_end(args);
+  return (size);
 }
--- main.h
+++ main.h
@@ -9,28 +9,21 @@
  * @t: The type to print
  * @f: The function to print
  */
-typedef struct print
+typedef struct _format
 {
 char *t;
 int (*f)(va_list);
-} print_t;
+} format;
 
 int _putchar(char c);
 int buffer(char);
+
+int handler(cont char *, va_list);
+int percent_handler(cont char *, va_list, int *);
+
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int print_c(va_list list);
+int print_s(va_list list);
 
 
 #endif
