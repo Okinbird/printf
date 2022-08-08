25efbfb40aa6fe6d5b3487a91ea5edf663a2b67c
Okinbird <okinyinka@yahoo.co.uk>
2022-08-08 15:47:39 -0700

    My twenty-two commit

--- handlers.c
+++ handlers.c
@@ -157,7 +157,8 @@ return (value);
  *         Otherwise - NULL.
  */
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-						      unsigned char, int, int, unsigned char)
+		unsigned char, int, int, unsigned char)
+
 {
 int i = 0;
 converter_t converters[] = {
