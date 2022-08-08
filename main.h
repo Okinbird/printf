4c1e39db98dbd8a8cb9d77e6e128feda288a93fe
Okinbird <okinyinka@yahoo.co.uk>
2022-08-08 14:11:35 -0700

    My twenty-two commit

--- main.h
+++ main.h
@@ -94,7 +94,7 @@ unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-							 unsigned char, int, int, unsigned char);
+							 unsigned char, int);
 
 /* Modifiers */
 unsigned int print_width(buffer_t *output, unsigned int printed,
