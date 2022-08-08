c121858f84cdec511e64b25e49dc6e15f0176b6f
Okinbird <okinyinka@yahoo.co.uk>
2022-08-08 12:40:52 -0700

    My twenty-two commit

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -16,19 +16,19 @@
 unsigned int convert_c(va_list args, buffer_t *output,
 		       unsigned char flags, int wid, int prec, unsigned char len)
 {
-  char c;
-  unsigned int ret = 0;
+char c;
+unsigned int ret = 0;
 
-  (void)prec;
-  (void)len;
+(void)prec;
+(void)len;
 
-  c = va_arg(args, int);
+c = va_arg(args, int);
 
-  ret += print_width(output, ret, flags, wid);
-  ret += _memcpy(output, &c, 1);
-  ret += print_neg_width(output, ret, flags, wid);
+ret += print_width(output, ret, flags, wid);
+ret += _memcpy(output, &c, 1);
+ret += print_neg_width(output, ret, flags, wid);
 
-  return (ret);
+return (ret);
 }
 
 /**
@@ -46,18 +46,18 @@ unsigned int convert_c(va_list args, buffer_t *output,
 unsigned int convert_percent(va_list args, buffer_t *output,
 			     unsigned char flags, int wid, int prec, unsigned char len)
 {
-  char percent = '%';
-  unsigned int ret = 0;
+char percent = '%';
+unsigned int ret = 0;
 
-  (void)args;
-  (void)prec;
-  (void)len;
+(void)args;
+(void)prec;
+(void)len;
 
-  ret += print_width(output, ret, flags, wid);
-  ret += _memcpy(output, &percent, 1);
-  ret += print_neg_width(output, ret, flags, wid);
+ret += print_width(output, ret, flags, wid);
+ret += _memcpy(output, &percent, 1);
+ret += print_neg_width(output, ret, flags, wid);
 
-  return (ret);
+return (ret);
 }
 
 /**
@@ -75,20 +75,20 @@ unsigned int convert_percent(va_list args, buffer_t *output,
 unsigned int convert_p(va_list args, buffer_t *output,
 		       unsigned char flags, int wid, int prec, unsigned char len)
 {
-  char *null = "(nil)";
-  unsigned long int address;
-  unsigned int ret = 0;
+char *null = "(nil)";
+unsigned long int address;
+unsigned int ret = 0;
 
-  (void)len;
+(void)len;
 
-  address = va_arg(args, unsigned long int);
-  if (address == '\0')
-    return (_memcpy(output, null, 5));
+address = va_arg(args, unsigned long int);
+if (address == '\0')
+return (_memcpy(output, null, 5));
 
-  flags |= 32;
-  ret += convert_ubase(output, address, "0123456789abcdef",
+flags |= 32;
+ret += convert_ubase(output, address, "0123456789abcdef",
 		       flags, wid, prec);
-  ret += print_neg_width(output, ret, flags, wid);
+ret += print_neg_width(output, ret, flags, wid);
 
-  return (ret);
+return (ret);
 }
