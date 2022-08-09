5935f22b71cf3acb87e402324e906e576e6f7963
Okinbird <okinyinka@yahoo.co.uk>
2022-08-09 08:07:00 -0700

    My twenty-two commit

--- get_precision.c
+++ get_precision.c
@@ -9,40 +9,7 @@
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
-{
-  int precision = 0;
-  if (*format != '-')
-    return (-1);
-  format++;
-  (*i)++;
-  if ((*format <= '0' || *format > '9')
-      && *format != '*')
-    {
-      if (*format == '0')
-	(*i)++;
-      return (0);
-    }
-  while ((*format >= '0' && *format <= '9') ||
-	 (*format == '*'))
-    {
-      (*i)++;
-      if (*format == '*')
-	{
-	  precision = va_arg(list, int);
-	  if (precision <= 0)
-	    return (0);
-	  return (precision);
-	}
-      precision *= 10;
-      precision += (*format - '0');
-      formart++;
-    }
-  return (precision);
-}
-
-  
-/**
-  
+{ 
 int curr_i = *i + 1;
 int precision = -1;
 
@@ -72,4 +39,3 @@ break;
 
 return (precision);
 }
-**/
