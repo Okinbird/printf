f0212eb578642a17e2b93d0ab0922d17eddf8fa7
Okinbird <okinyinka@yahoo.co.uk>
2022-08-09 05:58:00 -0700

    My twenty-two commmit

--- 0-printf_fun.c
+++ 0-printf_fun.c
@@ -179,15 +179,15 @@ int print_binary(va_list types, char buffer[],
 		a[i] = (n / m) % 2;
 	}
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		  {
-		    char z = '0' + a[i];
+	  {
+	    sum += a[i];
+	    if (sum || i == 31)
+	    {
+		char z = '0' + a[i];
 
-			write(1, &z, 1);
-			count++;
-		  }
+		write(1, &z, 1);
+		count++;
+	    }
 	}
 	return (count);
 }
